bb66f601e7372b7347a3d008dacfa05d7f66d7e5
Leonard Akpofure <la.shivure@gmail.com>
2023-09-22 10:28:33 +0100

    Final - live demo

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ function App() {
 				<h1 className="text-3xl md:text-6xl lg:text-8xl uppercase font-black mb-8">Maze Project</h1>
 	
 				<p className="text-base md:text-lg lg:text-2xl mb-8">A Doom 3D style JavaScript Raycaster using the browser's HTML5 Canvas for rendering.</p>
+
+				<div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
+				<a target="_blank" href="https://leonovis.github.io/mazetest/" className="hover:opacity-80 duration-150">CLICK here to view live demo</a>
+				</div>
+
+				<div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
+				<a target="_blank" href="https://github.com/leonovis/Maze-Project/tree/main/docs" className="hover:opacity-80 duration-150">CLICK here to view source code</a>
+				</div>
 			</main>
 			
 			<footer className="container mx-auto p-6">
